12f47602f286f3b3db2624691e27ce1d651ba87e
dev_danil <tinder12347@mail.ru>
2023-10-13 01:17:32 +0300

    fixed

--- trpo2.4/Program.cs
+++ trpo2.4/Program.cs
@@ -1,75 +1,24 @@
 ﻿//Дан текст из строных латинсвких букв, за которыми следует точка
 //Напечатать в алфавитном порядке все буквы, которые входят в этот текст по одному разу
 //Без использования цикла for, linq, встроенных функций
+using trpo2._4;
+
 try
 {
-    string text = "ABCDEFAAAA.";
+    string text = "BABCDEFAAAAC.";
+    Symbols symb = new Symbols();
+
     //Перевод строки в лист чаров
-    List<char> Listed(string text)
-    {
-        List<char> list = new List<char>();
-        int a = 0;
-        while (a != text.Length - 1)
-        {
-            list.Add(text[a++]);
-        }
-        return list;
-    }
-    var list = Listed(text);
+    var list = symb.Listed(text);
 
     //Сортировка списка по алфавитному порядку (пузырьковая)
-    List<char> Sort(List<char> list)
-    {
-        int a = 0;
-        char temp;
-        while (a != list.Count)
-        {
-            int b = a + 1;
-            while (b != list.Count)
-            {
-                if (list[a] > list[b])
-                {
-                    temp = list[a];
-                    list[a] = list[b];
-                    list[b] = temp;
-                }
-                b++;
-            }
-            a++;
-        }
-        return list;
-    }
-    list = Sort(list);
+    list = symb.Sort(list);
 
     //Удаление из списка элементов, которые входят в него не один раз
-    List<char> Delete(List<char> text)
-    {
-        List<char> list = new List<char>();
-        int a = 0;
-        while (a != text.Count())
-        {
-            int b = 0;
-            int c = 0;
-            while (c != text.Count())
-            {
-                if (text[a] == text[c])
-                {
-                    b++;
-                }
-                c++;
-            }
-            if (b < 2)
-            {
-                list.Add(text[a]);
-            }
-            a++;
-        }
-        return list;
-    }
-    list = Delete(list);
+    list = symb.Delete(list);
 
     Console.WriteLine("Входная строка \t\t" + text);
-    Console.WriteLine("Полученные буквы \t" + string.Join(",", list));
+    Console.WriteLine("Полученные буквы \t" + string.Join(",", list).ToUpper());
 }
 catch(Exception e)
 {
---
+++ trpo2.4/Symbols.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trpo2._4
+{
+    internal class Symbols
+    {
+        public List<char> Listed(string text)
+        {
+            List<char> list = new List<char>();
+            int a = 0;
+            while (a != text.Length - 1)
+            {
+                list.Add(text[a++]);
+            }
+            return list;
+        }
+        public List<char> Sort(List<char> list)
+        {
+            int a = 0;
+            char temp;
+            while (a != list.Count)
+            {
+                int b = a + 1;
+                while (b != list.Count)
+                {
+                    if (list[a] > list[b])
+                    {
+                        temp = list[a];
+                        list[a] = list[b];
+                        list[b] = temp;
+                    }
+                    b++;
+                }
+                a++;
+            }
+            return list;
+        }
+        public List<char> Delete(List<char> text)
+        {
+            List<char> list = new List<char>();
+            int a = 0;
+            while (a != text.Count())
+            {
+                int b = 0;
+                int c = 0;
+                while (c != text.Count())
+                {
+                    if (text[a] == text[c])
+                    {
+                        b++;
+                    }
+                    c++;
+                }
+                if (b < 2)
+                {
+                    list.Add(text[a]);
+                }
+                a++;
+            }
+            return list;
+        }
+
+    }
+}
