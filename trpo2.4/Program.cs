245c27a6b2f1fffa2eb61e6eb04d7ff3fdb3fdda
dev_danil <tinder12347@mail.ru>
2023-10-12 17:56:51 +0300

    finish

--- trpo2.4/Program.cs
+++ trpo2.4/Program.cs
@@ -1,78 +1,79 @@
 ﻿//Дан текст из строных латинсвких букв, за которыми следует точка
 //Напечатать в алфавитном порядке все буквы, которые входят в этот текст по одному разу
 //Без использования цикла for, linq, встроенных функций
-
-using Microsoft.VisualBasic;
-
-string text = "ABCDEFAAAA.";
-
-//Перевод строки в лист чаров
-List<char> Listed(string text)
+try
 {
-    List<char> list = new List<char>();
-    int a = 0;
-    while (a != text.Length - 1)
+    string text = "ABCDEFAAAA.";
+    //Перевод строки в лист чаров
+    List<char> Listed(string text)
     {
-        list.Add(text[a++]);
-    }
-    return list;
-}
-
-var list = Listed(text);
-
-//Сортировка списка по алфавитному порядку (пузырьковая)
-List<char> Sort(List<char> list)
-{
-    int a = 0;
-    char temp;
-    while (a != list.Count)
-    {
-        int b = a + 1;
-        while (b != list.Count)
+        List<char> list = new List<char>();
+        int a = 0;
+        while (a != text.Length - 1)
         {
-            if (list[a] > list[b])
-            {
-                temp = list[a];
-                list[a] = list[b];
-                list[b] = temp;
-            }
-            b++;
+            list.Add(text[a++]);
         }
-        a++;
+        return list;
     }
-    return list;
-}
-list = Sort(list);
+    var list = Listed(text);
 
-//Удаление из списка элементов, которые входят в него не один раз
-List<char> Delete(List<char> text)
-{
-    List<char> list = new List<char>();
-    int a = 0;
-    while (a != text.Count())
+    //Сортировка списка по алфавитному порядку (пузырьковая)
+    List<char> Sort(List<char> list)
     {
-        int b = 0;
-        int c = 0;
-        while (c != text.Count())
+        int a = 0;
+        char temp;
+        while (a != list.Count)
         {
-            if (text[a] == text[c])
+            int b = a + 1;
+            while (b != list.Count)
             {
+                if (list[a] > list[b])
+                {
+                    temp = list[a];
+                    list[a] = list[b];
+                    list[b] = temp;
+                }
                 b++;
             }
-            c++;
+            a++;
         }
-        if (b < 2)
-        {
-            list.Add(text[a]);
-        }
-        a++;
+        return list;
     }
-    return list;
+    list = Sort(list);
+
+    //Удаление из списка элементов, которые входят в него не один раз
+    List<char> Delete(List<char> text)
+    {
+        List<char> list = new List<char>();
+        int a = 0;
+        while (a != text.Count())
+        {
+            int b = 0;
+            int c = 0;
+            while (c != text.Count())
+            {
+                if (text[a] == text[c])
+                {
+                    b++;
+                }
+                c++;
+            }
+            if (b < 2)
+            {
+                list.Add(text[a]);
+            }
+            a++;
+        }
+        return list;
+    }
+    list = Delete(list);
+
+    Console.WriteLine("Входная строка \t\t" + text);
+    Console.WriteLine("Полученные буквы \t" + string.Join(",", list));
 }
-
-list = Delete(list);
-
-Console.WriteLine("Входная строка \t\t" + text);
-Console.WriteLine("Полученные буквы \t" + string.Join(",", list));
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
 
 
